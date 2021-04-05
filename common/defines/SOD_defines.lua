3114b6d7379e4f2a8f2359ace753a81203d804a1
kstar234 <32802115+kstar234@users.noreply.github.com>
2021-04-05 17:56:21 -0500

    map still broken apparently...arabs get generic tree to subsitute for future content

--- common/defines/SOD_defines.lua
+++ common/defines/SOD_defines.lua
@@ -1,1 +1,5 @@
 NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 24;
+NDefines.NDiplomacy.MAX_TRUST_VALUE = 200;
+NDefines.NDiplomacy.MIN_TRUST_VALUE = -200;
+NDefines.NDiplomacy.MAX_OPINION_VALUEE = 200;
+NDefines.NDiplomacy.MIN_OPINION_VALUE = -200;
